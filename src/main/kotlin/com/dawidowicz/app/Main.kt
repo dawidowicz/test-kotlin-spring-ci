678b9228c665550abb0528f7e49f9c91802cfc62
Maciej Dawidowicz <md.dawidowicz@gmail.com>
2018-12-22 16:02:43 +0100

    hello world

--- src/main/kotlin/com/dawidowicz/app/Main.kt
+++ src/main/kotlin/com/dawidowicz/app/Main.kt
@@ -2,10 +2,21 @@ package com.dawidowicz.app
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 open class Main
 
 fun main(args: Array<String>) {
     SpringApplication.run(Main::class.java)
+}
+
+@RestController
+open class HelloWorldRestController {
+
+    @GetMapping("/**", "/", "**", "*", "/*")
+    fun helloWorld(): String {
+        return "Hello World!"
+    }
 }
